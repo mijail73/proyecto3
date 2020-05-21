ad5bf15438a1caa8ace3fcc64c35dbdce0be5f1f
mijail73 <mijail.iso@ciencias.unam.mx>
2020-05-21 00:17:51 -0500

    Parte

---
+++ src/proyecto3icc/Parte.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyecto3icc;
+
+/**
+ *
+ * @author mijail
+ */
+public class Parte {
+    private String nombre;
+    private int cantidad;
+    private String color;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(int cantidad) {
+        this.cantidad = cantidad;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+    
+}
